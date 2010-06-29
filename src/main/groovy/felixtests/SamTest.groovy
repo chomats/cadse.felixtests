c46bc44dc557ed3d803a8d191407087eb9c8f52f
Stephane Chomat <Stephane.chomat@imag.fr>
2010-06-29 15:47:12 +0200

    fix import

--- src/main/groovy/felixtests/SamTest.groovy
+++ src/main/groovy/felixtests/SamTest.groovy
@@ -10,14 +10,13 @@ public class SamTest extends FelixTestCase {
 		String vId = "2.3.0-SNAPSHOT";
 		
 		/* Libraries */
-		run.addBundelInM2("forfelix", gId, "forfelix", vId)
 		
 		run.addBundelInM2("Model.Workspace.sam.composite", gId, "Model.Workspace.sam.composite", vId)
 		run.addBundelInM2("Model.Workspace.sam.core", gId, "Model.Workspace.sam.core", vId)
 		run.addBundelInM2("Selecta.constraint.language.extension", 'fr.imag.adele.cadse', "Selecta.constraint.language.extension", vId)
+		
 		run.addBundelInM2("fr.imag.adele.cadse.test.runtime", gId, "fr.imag.adele.cadse.test.runtime", vId)
 		
-		run.addBundelInM2('org.eclipse.jdt.junit4.runtime', 'org.eclipse', 'org.eclipse.jdt.junit4.runtime','1.1.100-v20100526.0800')
 		
 		/* TEST */
 		CadseTestPart tp = addTestPart(null, 'SamTest',
